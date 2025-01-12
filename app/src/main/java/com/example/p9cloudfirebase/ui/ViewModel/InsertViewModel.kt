5078ff59f471e5027dcc57dd3a6ac4963bc8aba4
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-01-13 00:41:52 +0700

    Membuat data class FormErrorState dan menambahkan komentar

--- app/src/main/java/com/example/p9cloudfirebase/ui/ViewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/p9cloudfirebase/ui/ViewModel/InsertViewModel.kt
@@ -2,6 +2,20 @@ package com.example.p9cloudfirebase.ui.ViewModel
 
 import com.example.p9cloudfirebase.Model.Mahasiswa
 
+data class FormErrorState(
+    val nim:String? = null,
+    val nama:String? = null,
+    val alamat:String? = null,
+    val gender:String? = null,
+    val kelas:String? = null,
+    val angkatan:String? = null
+){
+    fun isValid(): Boolean{
+        return nim == null && nama == null && gender == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
 //data class variable yang menyimpan data input form
 data class MahasiswaEvent(
     val nim:String = "",
@@ -12,6 +26,7 @@ data class MahasiswaEvent(
     val angkatan:String = ""
 )
 
+//Menyimpan input form ke dalam entity
 fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
