57be3ce2a08393607abaeccb0f2327b7bf44c598
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-01-13 00:37:39 +0700

    Membuat fun MahasiswaEvent

--- app/src/main/java/com/example/p9cloudfirebase/ui/ViewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/p9cloudfirebase/ui/ViewModel/InsertViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.p9cloudfirebase.ui.ViewModel
 
+import com.example.p9cloudfirebase.Model.Mahasiswa
+
 //data class variable yang menyimpan data input form
 data class MahasiswaEvent(
     val nim:String = "",
@@ -8,4 +10,13 @@ data class MahasiswaEvent(
     val gender:String = "",
     val kelas:String = "",
     val angkatan:String = ""
+)
+
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    gender = gender,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
 )
