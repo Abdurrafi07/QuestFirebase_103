ca3e753249880ee755c828af08688ac05d9119d7
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-01-13 00:34:40 +0700

    Membuat file InsertViewModel.kt dan data class MahasiswaEvent

---
+++ app/src/main/java/com/example/p9cloudfirebase/ui/ViewModel/InsertViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.p9cloudfirebase.ui.ViewModel
+
+//data class variable yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim:String = "",
+    val nama:String = "",
+    val alamat:String = "",
+    val gender:String = "",
+    val kelas:String = "",
+    val angkatan:String = ""
+)
